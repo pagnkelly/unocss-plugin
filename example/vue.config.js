7cfa107d08b55078b44990918a3884a1bfe52af6
pagnkelly <632228198@qq.com>
2023-05-22 11:38:03 +0800

    feat: example

---
+++ example/vue.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('@vue/cli-service')
+const MpxUnocssPlugin = require('@mpxjs/unocss-plugin')
+module.exports = defineConfig({
+  pluginOptions: {
+    mpx: {
+      srcMode: 'wx',
+      plugin: {
+        hackResolveBuildDependencies: ({ files, resolveDependencies }) => {
+          const path = require('path')
+          const packageJSONPath = path.resolve('package.json')
+          if (files.has(packageJSONPath)) files.delete(packageJSONPath)
+          if (resolveDependencies.files.has(packageJSONPath)) {
+            resolveDependencies.files.delete(packageJSONPath)
+          }
+        }
+      },
+      loader: {}
+    }
+  },
+  /**
+   * 如果希望node_modules下的文件时对应的缓存可以失效，
+   * 可以将configureWebpack.snap.managedPaths修改为 []
+   */
+  configureWebpack(config) {
+    return {
+      plugins: [new MpxUnocssPlugin()]
+    }
+  }
+})
