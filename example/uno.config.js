8153ec45caa214e18227a9a821796a6feb7632d6
pagnkelly <632228198@qq.com>
2023-06-01 17:55:37 +0800

    feat: log

--- example/uno.config.js
+++ example/uno.config.js
@@ -1,11 +1,8 @@
 import {
   defineConfig,
-  // presetAttributify,
-  presetUno,
   transformerVariantGroup
 } from 'unocss'
 import { presetBase } from '@mpxjs/unocss-base'
-console.log(presetBase(), 'presetBase()')
 export default defineConfig({
   presets: [
     presetBase()
